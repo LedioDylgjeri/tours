0813c0bb68f87632df0b6688aad8748fffdeb456
Ledio Dylgjeri <lediodylgjeri@gamil.com>
2023-03-21 21:05:47 -0400

    add toggle functionality

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,11 @@ const App = () => {
   const [isLoading, setIsLoading] = useState(true)
   const [tours, setTours] = useState([])
 
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
+  }
+
   const fetchTours = async () => {
     setIsLoading(true)
     try {
@@ -32,7 +37,7 @@ const App = () => {
 
 
   return  <main>
-      <Tours tours={tours}/>
+      <Tours tours={tours} removeTour={removeTour}/>
     </main>;
 };
 export default App;
